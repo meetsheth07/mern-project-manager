f12a0702eb23e02d08e991e7592ebc5160b73964
meetsheth07 <meetsheth698@gmail.com>
2026-06-11 11:25:57 +0530

    rectified username--error

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("starting Backend server...");
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});    
+
+let appUsername = process.env.APP_USERNAME;
+let password = process.env.APP_PASSWORD;
+let port = process.env.APP_PORT;
+console.log(appUsername);
+console.log("starting Backend server of this course");
